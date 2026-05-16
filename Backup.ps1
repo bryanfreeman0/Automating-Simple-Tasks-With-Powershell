b2f3a4bae8b4ba18489d9cca9928e5028929f8b5
bryanfreeman0 <bryanfreeman0@gmail.com>
2026-05-16 17:53:20 -0400

    Add files via upload

--- Backup.ps1
+++ Backup.ps1
@@ -1,2 +1,2 @@
 Get-ChildItem -Path "C:\Users\Bryan\Documents" -File | Copy-Item -Destination "Z:\Backup\" -Force
-Get-ChildItem -Path "C:\Users\Bryan\ Pictures" -File | Copy-Item -Destination "Z:\Backup\" -Force
+Get-ChildItem -Path "C:\Users\Bryan\Pictures" -File | Copy-Item -Destination "Z:\Backup\" -Force
