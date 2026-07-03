1ec723422482ab8192076fb53f2a020787debd48
bryanfreeman0 <bryanfreeman0@gmail.com>
2026-07-03 12:25:15 -0400

    Modify Clear Files.ps1 to include all user Downloads
    Updated the script to clear Downloads for all users.

--- Clear Files.ps1
+++ Clear Files.ps1
@@ -2,6 +2,6 @@ Remove-Item "$env:TEMP\*"        -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item "$env:TMP\*"         -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item "C:\Windows\Temp\*"  -Recurse -Force -ErrorAction SilentlyContinue
 
-Remove-Item "$env:USERPROFILE\Downloads\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "C:\Users\*\Downloads\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
